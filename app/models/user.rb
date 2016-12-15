14fa28f3340f8e79b835bfc6225f320daca18715
cristianeing <cristian.eing@gmail.com>
2016-12-15 15:44:51 -0300

    Arregle Sign in con Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
-  
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name: data["name"],
+          provider:access_token.provider,
+          email: data["email"],
+          uid: access_token.uid ,
+          password: Devise.friendly_token[0,20],
+        )
+      end
+    end
+  end
   
   def self.find_for_oauth(auth, signed_in_resource = nil)
 
