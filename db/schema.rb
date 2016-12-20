a8cd4ab64bd65a607f3e9714db400409b7f3d43b
cristianeing <cristian.eing@gmail.com>
2016-12-20 16:05:45 -0300

    Solo se le hizo un migracion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161219193659) do
 
   create_table "activities", force: :cascade do |t|
@@ -43,7 +42,6 @@ ActiveRecord::Schema.define(version: 20161219193659) do
 
   add_index "follows", ["followable_id", "followable_type"], name: "fk_followables", using: :btree
   add_index "follows", ["follower_id", "follower_type"], name: "fk_follows", using: :btree
-
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
