9b7d3607fb679d19cf34d83e97f7f04a522f1faf
cristianeing <cristian.eing@gmail.com>
2016-12-26 13:05:49 -0300

    Agregue el boton Follow y Unfollow con sus respectivos atributos

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219193659) do
-
+ActiveRecord::Schema.define(version: 20161220184608) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
